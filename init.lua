948543a664eac8ac2e5241627a79463bd8911ff0
Bryce Turley <turleyb2@gmail.com>
2025-01-10 10:06:37 -0800

    autocomplete now works

--- init.lua
+++ init.lua
@@ -30,6 +30,35 @@ require("mason-lspconfig").setup {
 	},
 }
 
+-- configure autocompletion using 'cmp' to provide 'luasnip' suggestions
+require("cmp").setup(
+	{
+        -- REQUIRED - you must specify a snippet engine
+        snippet = {
+            expand = function(args)
+                require('luasnip').lsp_expand(args.body)
+            end,
+        },
+        window = {
+          -- completion = cmp.config.window.bordered(),
+          -- documentation = cmp.config.window.bordered(),
+        },
+        mapping = require("cmp").mapping.preset.insert({
+          ['<C-b>'] = require("cmp").mapping.scroll_docs(-4),
+          ['<C-f>'] = require("cmp").mapping.scroll_docs(4),
+          ['<C-Space>'] = require("cmp").mapping.complete(),
+          ['<C-e>'] = require("cmp").mapping.abort(),
+          ['<CR>'] = require("cmp").mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }),
+        sources = require("cmp").config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' }, -- For luasnip users.
+        }, {
+          { name = 'buffer' },
+        }),
+	}
+)
+
 
 -- choose color
 -- vim.o.background = "light" -- or "light" for light mode on 'gruvbox'
