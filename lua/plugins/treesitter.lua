950f2a2b02345e3590b512466379ce984719ef46
Bryce Turley <turleyb2@gmail.com>
2025-01-09 17:25:58 -0800

    added lspconfig

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,25 @@ return {
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "html", "rust", "python", "json", "cpp" },
+          ensure_installed = { 
+				"c",
+				"cpp",
+				"go",
+				"html",
+				"java",
+				"json",
+				"lua",
+				"markdown",
+				"query",
+				"rust",
+				"python",
+				"sql",
+				"terraform",
+				"toml", 
+				"vim",
+				"vimdoc",
+				"yaml",
+			},
           sync_install = false,
           highlight = { enable = true },
           indent = { enable = true },  
