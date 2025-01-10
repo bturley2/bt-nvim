c053ea9975f4c07e336df05873a47b926c274844
Bryce Turley <turleyb2@gmail.com>
2025-01-10 07:43:32 -0800

    lsp is tidy now

--- init.lua
+++ init.lua
@@ -1,37 +1,34 @@
 require("config.lazy")
+
+local supported_languages = {
+    "bashls",
+	"clangd",
+	"gopls",
+	-- "harper_ls", -- provides grammar support, faster alternative to grammarly
+	-- "java_language_server",
+	"jsonls",
+	"lua_ls",
+	"marksman",
+	"pyright",
+	"sqls",
+	"taplo", -- toml
+	"rust_analyzer",
+	"yamlls",
+}
+
 require("mason").setup()
 require("mason-lspconfig").setup {
 	-- this step INSTALLS the following language servers
-	ensure_installed = {
-			"bashls",
-			"clangd",
-			"gopls",
-			-- "harper_ls", -- provides grammar support, faster alternative to grammarly
-			-- "java_language_server",
-			"jsonls",
-			"lua_ls",
-			"marksman",
-			"pyright",
-			"sqls",
-			"taplo", -- toml
-			"rust_analyzer",
-			"yamlls",
-	},
+	ensure_installed = supported_languages,
 	automatic_installation = true,
+	-- this step STARTS each language server on localhost
+	handlers = {
+		function(server_name)
+			local server = supported_languages[server_name] or {}
+			require("lspconfig")[server_name].setup(server)
+		end,
+	},
 }
-
--- this step STARTS the following language servers on localhost
-require("lspconfig").bashls.setup {}
-require("lspconfig").clangd.setup {}
-require("lspconfig").gopls.setup {}
-require("lspconfig").jsonls.setup {}
-require("lspconfig").lua_ls.setup {}
-require("lspconfig").marksman.setup {}
-require("lspconfig").pyright.setup {}
-require("lspconfig").sqls.setup {}
-require("lspconfig").taplo.setup {}
-require("lspconfig").rust_analyzer.setup {}
-require("lspconfig").yamlls.setup {}
 
 
 -- choose color
@@ -47,4 +44,33 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find f
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+vim.g.have_nerd_font = true
+vim.opt.breakindent = true
+-- Enable mouse mode, can be useful for resizing splits
+vim.opt.mouse = 'a'
+vim.opt.cursorline = true
+
+-- Make line numbers default, or add relative line numbers
+vim.opt.number = true
+-- vim.opt.relativenumber = true
+
+
+-- Highlight when yanking (copying) text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+vim.o.tabstop = 4
+vim.o.expandtab = true
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+
+
+
+
 
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,7 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+	'nvim-telescope/telescope.nvim', tag = '0.1.8',
+	dependencies = {
+		'nvim-lua/plenary.nvim' ,
+		'nvim-telescope/telescope-ui-select.nvim', -- optional, makes telescope default for other things in vim
+	}
 }
